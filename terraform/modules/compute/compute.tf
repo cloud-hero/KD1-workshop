e1f277e42df1c5d3460c96797317cad0a44c4c95
Andrei Petrescu <andrei.petrescu@cloudhero.io>
2020-12-13 23:47:18 +0200

    first commit

---
+++ terraform/modules/compute/compute.tf
@@ -0,0 +1,22 @@
+resource "google_compute_instance" "node" {
+  name         = var.name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-2004-lts"
+      size  = "100"
+    }
+  }
+
+
+  network_interface {
+    network     = var.network
+    subnetwork  = var.subnetwork
+  }
+
+  metadata = {
+    ssh-keys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
+  }
+}
---
+++ terraform/modules/compute/variables.tf
@@ -0,0 +1,22 @@
+variable "name" {
+    type = string
+}
+
+variable "machine_type" {
+    type = string
+}
+
+variable "zone" {
+    type = string
+}
+
+variable "gce_ssh_pub_key_file" {
+    type = string
+}
+
+variable "gce_ssh_user" {
+    type = string
+}
+
+variable "network" {}
+variable "subnetwork" {}
